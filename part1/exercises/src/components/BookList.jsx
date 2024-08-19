11ccd084a67036d54f996f89ec32c64b74b91d68
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-19 16:12:14 -0500

    changed assigned a new heading to pageTitle and added 3 images to BookList function

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books To Read List";
+   let book1 = "https://m.media-amazon.com/images/M/MV5BMWE1NDM1ZGMtM2E2NS00NTllLTgwMjEtYjIxNWYzMzExMzZkXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg";
+   let book2 = "https://m.media-amazon.com/images/M/MV5BYjY4MjNjZDEtNmJkMy00MjA4LWI3NmMtODFjZDM3ZTdmOWY0XkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_.jpg";
+   let book3 = "https://m.media-amazon.com/images/M/MV5BNGUzN2M0YTEtZTk4OC00MGI1LWI2MGYtN2QyM2M1NzU3OGM0XkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_FMjpg_UX1000_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Apothecary Diaries" height={300}/>
+         <img src={book2} alt="Raven of the Inner Palace" height={300}/>
+         <img src={book3} alt="Sugar Apple Fairy Tale" height={300}/>
       </div>      
    );
 }
