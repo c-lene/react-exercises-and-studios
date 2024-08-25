1ee2c90a9e926216f276ce66f7f455bbdb927ee4
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-25 02:36:47 -0500

    CT completed - added Author name, image, link

--- part2/studio/src/components/AuthorInfo.jsx
+++ part2/studio/src/components/AuthorInfo.jsx
@@ -1,11 +1,40 @@
+import recipedata from "./recipe.json";
+import './styling.css';
+
 function AuthorInfo() {
-   return;
+  const recipeAuthor = (
+    <div>
+      {recipedata[0].author}
+    </div>
+  );
+
+  const recipeAuthorImage = (
+    <div>
+      <img 
+        src={recipedata[0].authorImage} 
+        alt={recipedata[0].author}
+        className="authorImage" 
+      />
+    </div>
+  )
+
+  const recipeWebsite = (
+    <a href={recipedata[0].website} target="_blank">Recipe Link</a>
+  )
+  
+  return(
+    <div>
+      {recipeAuthorImage}
+      {recipeAuthor}
+      {recipeWebsite}
+    </div>
+  );
  }
  
  export default AuthorInfo;
  
  //import styles.css
- //import json file for author info
+ //import json file for author info - DONE
  //image, name, website
  //css for styling image
  
