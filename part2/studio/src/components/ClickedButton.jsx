0467526b74c657a694a0c976a7ed972376d9ba60
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-25 02:38:53 -0500

    CT completed - added alert when clicked

--- part2/studio/src/components/ClickedButton.jsx
+++ part2/studio/src/components/ClickedButton.jsx
@@ -1,9 +1,19 @@
+import "./styling.css";
+import "./recipe.json";
+
 function ClickedButton() {
-  return;
+  function handleClick() {
+    alert("You are removing this pin!");
+  }
+  return (
+    <button id="clickedButton" onClick={handleClick}>
+      Saved
+    </button>
+  );
 }
 
 export default ClickedButton;
 
-//create alert to inform user that the pin has been saved
-//create button 
-//import styling and assign correct className clickedButton
+//create alert to inform user that the pin has been saved - DONE
+//create button - DONE
+//import styling and assign correct className clickedButton - DONE
