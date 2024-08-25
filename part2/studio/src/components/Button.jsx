c96c2d091fe3cfcce01d6ddb097c5083ae7b8950
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-25 02:37:49 -0500

    CT completed - updated save/clicked button

--- part2/studio/src/components/Button.jsx
+++ part2/studio/src/components/Button.jsx
@@ -1,10 +1,23 @@
-function Button() {
-   return;
+//need to import SaveButton and ClickedButton
+import SaveButton from "./SaveButton";
+import ClickedButton from "./ClickedButton";
+
+//import styling
+import "./styling.css";
+import "./recipe.json";
+
+
+function Button(props) {
+  const saveButton = props.saveButton;
+
+  //create conditional for these buttons
+  let button = saveButton ? SaveButton() : ClickedButton();
+   return button;
  }
  
  export default Button;
  
- //need to import SaveButton and ClickedButton
- //create conditional for these buttons
- //import styling
+
+ 
+ 
  
