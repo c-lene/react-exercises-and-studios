a719c5950d337b1f101b5534e502c26c01be2842
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-25 02:42:43 -0500

    CT completed - using props to update star ratings

--- part2/studio/src/components/RateARecipe.jsx
+++ part2/studio/src/components/RateARecipe.jsx
@@ -1,9 +1,20 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe() {
+
+function RateARecipe(props) {
+  
+  //conditional to render stars based on a number provided in App.js
+  let stars = props.rating >= 1 && props.rating <= 5 ? GiveRating(props) : null;
+  
   return stars;
 }
 
+function GiveRating(props) {
+  return <h3>{stars[props.rating - 1]}</h3>;
+}
+
+
 export default RateARecipe;
 
-//conditional to render stars based on a number provided in App.js 
+
+
