c0ba209b44174e3db895906aa286f7eaa7bdd853
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-25 02:45:18 -0500

    CT completed - added recipeImage from json

--- part2/studio/src/components/RecipeImage.jsx
+++ part2/studio/src/components/RecipeImage.jsx
@@ -1,9 +1,21 @@
+//import json file for the data
+import recipedata from "./recipe.json";
+
 function RecipeImage() {
-   return;
+   return (
+    <div>
+      <img 
+        //apply css for className recipeImage
+        className="recipeImage"
+        src={recipedata[0].recipeImage}
+        alt={recipedata[0].name} 
+      />
+    </div>
+   );
  }
  
  export default RecipeImage;
  
- //import json file for the data
- //apply css for className recipeImage
+ 
+ 
  
