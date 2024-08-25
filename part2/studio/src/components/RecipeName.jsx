e926890a0381a169697312f071f0d42ea6299b88
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-25 02:46:28 -0500

    CT completed - Added recipeName from json

--- part2/studio/src/components/RecipeName.jsx
+++ part2/studio/src/components/RecipeName.jsx
@@ -1,9 +1,20 @@
 import recipedata from "./recipe.json";
 
 function RecipeName() {
-  return recipedata;
+
+  //import return the name of the recipe as a level 1 header
+  const recipeName = (
+    <div>
+      <h1>{recipedata[0].name}</h1>
+    </div>
+  );
+  
+  return(
+    <div>
+      {recipeName}
+    </div>
+  );
 }
 
 export default RecipeName;
 
-//import return the name of the recipe as a level 1 header
