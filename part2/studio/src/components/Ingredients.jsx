0bfbe0c16600f9a2b7d0e460591fbb4bd3673953
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-25 02:41:36 -0500

    CT completed - used mapping to list each ingredient

--- part2/studio/src/components/Ingredients.jsx
+++ part2/studio/src/components/Ingredients.jsx
@@ -1,9 +1,24 @@
+//import json file for the data - DONE
+import recipedata from "./recipe.json";
+
+
+
 function IngredientList() {
-   return;
+  
+  //use a nested map to get inside the inner array
+  const ingredientItem = recipedata[0].ingredients.map((ingredient, index) => {
+    return (
+      <li key={index}>{ingredient}</li>
+    );
+  });
+  
+  return(
+    <div>
+      <h3>Ingredients</h3>
+      <ul>{ingredientItem}</ul>
+    </div>
+   );
  }
  
+ 
  export default IngredientList;
- 
-//import json file for the data
-//use a nested map to get inside the inner array
- 
