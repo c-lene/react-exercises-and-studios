9cc8257886f4f618f5028942e8250135391cb67f
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-26 23:32:02 -0500

    CT completed - updated 3 fun facts about Windowsill Gardening

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Windosill Herb Gardening</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Windowsill gardens adds color and texture to your home.</li>
+            <li>Try to avoid windowsills near heating vents or cooking appliances as it will dcreease the air of proper humidity.</li>
+            <li>Most plants grow best in windows facing East or Southeast to benefit from morning light.</li>
          </ol>
       </div>
    );
