eb3ff26f2c34286555ece191272374acf994710e
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-26 23:34:36 -0500

    CT completed - Updated html of object keys

--- part3/exercises/src/components/Projects.jsx
+++ part3/exercises/src/components/Projects.jsx
@@ -23,7 +23,7 @@ export default function MyProjects() {
       <button onClick={handleClick}>
         Next
       </button>
-      <h2>{project.canvas} by {project.designer}
+      <h2><em>{project.species}</em> known as {project.herb}
       </h2>
       <img src={project.photoUrl} alt={project.alt} />
     </div>
