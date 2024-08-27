f835f127f857be9d75c301edd9f89ad6246d95ab
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-27 18:15:46 -0500

    CT completed - updated board variable, handleChange(), used map() to iterate boards variable

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const [boardName, setName] = useState("no boards yet!");
+
+   const boards = [
+      {
+         label: "Asian",
+         value: "Asian Recipes",
+      }, 
+      {
+         label: "Western",
+         value: "Western Recipes",
+      },
+      {
+         label: "Dump & Go",
+         value: "Dump & Go Recipes",
+      },
+   ];
 
    const handleChange = (event) => {
+      setName((boardName) => event.target.value)
    }
+
+   const selectBoard = boards.map((board) => {
+      return (
+         <option value={board.value}>{board.label}</option>
+      )
+   });
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {selectBoard}
       </select>
 
       <p>Saved to {boardName}!</p>
