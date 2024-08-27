6d20e5e409a366c656300244e29b7a10a65c6cee
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-27 18:19:07 -0500

    declared & initialized variables using useState, updated handleChange & handleSubmit, used conditional rendering to update HTML

--- part3/studio/src/components/StatusChange.jsx
+++ part3/studio/src/components/StatusChange.jsx
@@ -2,23 +2,32 @@ import { useState } from 'react';
 
 export default function StatusChange () {
 
+   const [notes, setNotes] = useState("");
+   const [recipeStatus, setStatus] = useState(false);
+   
    const handleChange = (event) => {
+      setNotes((notes) => event.target.value);
    }
 
    const handleSubmit = (event) => {
+      event.preventDefault();
+      setStatus((recipeStatus) => true);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
+         
+
+         
          <form onSubmit={handleSubmit}>
             <label>Have you tried this recipe? Add your notes here: <input type="text" value={notes} onChange={handleChange} />
             </label>
             <input type="submit" />
          </form>
 
-         <p>My Recipe Notes aren't here!</p>
+         <p>My Recipe Notes: {notes}</p>
 
-         <p>I have not tried this recipe!</p>
+         <p>I {recipeStatus ? "HAVE" : "have NOT"} tried this recipe!</p>
       </div>
    );
 }
